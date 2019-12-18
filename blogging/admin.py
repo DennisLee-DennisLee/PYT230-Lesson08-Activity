c4ec3d34a251562f9c75c445a917e8c65c235129
Dennis Lee <v-dele@microsoft.com>
2019-12-18 00:09:43 -0800

    Adding back the lower-level files from the L7 assignment.

---
+++ blogging/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from blogging.models import Post, Category , PostAdmin , CategoryAdmin
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
