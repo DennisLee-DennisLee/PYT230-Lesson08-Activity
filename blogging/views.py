7fbf1f9e93c87714348dc31d207342d5cfe8f9ec
Dennis Lee <v-dele@microsoft.com>
2019-12-27 18:05:58 -0800

    Ready for submission.

--- blogging/views.py
+++ blogging/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User, Group
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseRedirect, Http404
-from django.template import loader
-from blogging.models import Post
+from django.utils import timezone
+from rest_framework import viewsets
+from blogging.models import Post, Category
+from blogging.serializers import UserSerializer, GroupSerializer, PostSerializer, CategorySerializer
+from blogging.forms import PostForm, CategoryForm
 
 def list_view(request):
     published = Post.objects.exclude(published_date__exact=None)
@@ -18,12 +22,30 @@ def detail_view(request, post_id):
     context = {'post': post}
     return render(request, 'blogging/detail.html', context)
 
-def stub_view(request, *args, **kwargs):
-    body = "Stub View\n\n"
-    if args:
-        body += "Args:\n"
-        body += "\n".join(["\t%s" % a for a in args])
-    if kwargs:
-        body += "Kwargs:\n"
-        body += "\n".join(["\t%s: %s" % i for i in kwargs.items()])
-    return HttpResponse(body, content_type="text/plain")
+def add_model(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            model_instance = form.save(commit=False)
+            model_instance.timestamp = timezone.now()
+            model_instance.save()
+            return redirect('/')
+    else:
+        form = PostForm()
+        return render(request, "my_template.html", {'form': form})
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.exclude(published_date__exact=None).order_by('-published_date')
+    serializer_class = PostSerializer
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
